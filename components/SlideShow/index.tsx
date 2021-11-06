511c5db66151cbb2946bfae50b99c19d29e1a20e
Steven Casto <castrostevenyc@gmail.com>
2021-11-06 19:11:30 -0400

    Scroll Images work

--- components/SlideShow/index.tsx
+++ components/SlideShow/index.tsx
@@ -1,43 +1,54 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-
+import SwiperCore, { Swiper, SwiperSlide } from "swiper/react";
+import style from "./style.module.css";
 import "swiper/css";
+import Button from "../Button";
 
+// https://swiperjs.com/react
 const SlideShow = () => {
   return (
     <Swiper
       className="h-full"
-      spaceBetween={50}
+      // spaceBetween={50}
       slidesPerView={1}
+      // loop={true}
+      autoplay={{ delay: 10000 }}
       onSlideChange={() => console.log("slide change")}
       onSwiper={(swiper) => console.log(swiper)}
     >
       <SwiperSlide>
-        {/* Layer 1 - Black WH-full */}
-        <div className="bg-black h-full w-full ">
-          {/* Layer 2 Img & opacity */}
-          <div
-            className="
-            h-full w-full 
-            bg-slide1 bg-center bg-cover   
-            bg-no-repeat opacity-40
-            relative z-20
-            "
-          >
-            {/* TEXT WHITE */}
-            <div
-              className="
-              text-white flex justify-center 
-              items-center h-full w-full
-                "
-            >
-              <span className="font-extrabold text-2xl">FUCK</span>
+        <div style={style} className={`h-full w-full ${style.slide1} `}>
+          <div className="absolutetext-white flex justify-center items-center h-full w-full ">
+            <div className="text-white text-left container mx-auto p-5 flex flex-col space-y-10 ">
+              <p className="text-3xl font-semibold">
+                ENTERPRISE SOFTWARE <br />
+                DEVELOPMENT
+              </p>
+              <p>
+                Let our team of Certified Engineers and Designers be on your
+                team.
+              </p>
+              <Button className="w-3/4 md:w-1/4">Read our case studies</Button>
             </div>
           </div>
         </div>
       </SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>
+        <div style={style} className={`h-full w-full ${style.slide2} `}>
+          <div className="absolutetext-white flex justify-center items-center h-full w-full ">
+            <div className="text-white text-left container mx-auto p-5 flex flex-col space-y-10 ">
+              <p className="text-3xl font-semibold">
+                MOBILE APPLICATION
+                <br />
+                DEVELOPMENT
+              </p>
+              <p>One Codebase for IOS, Android & Windows</p>
+              <Button className="w-3/4 md:w-1/4">See our App Portfolio</Button>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      {/* <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide> */}
     </Swiper>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import * as ga from '../lib/ga'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as ga from "../lib/ga";
+import SwiperCore, { Autoplay } from "swiper";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
+  SwiperCore.use([Autoplay]);
+
+  const router = useRouter();
   useEffect(() => {
-    const handleRouteChange = (url:string) => {
-      ga.pageview(url)
-    }
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
     //When the component is mounted, subscribe to router changes
     //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on("routeChangeComplete", handleRouteChange);
 
     // If the component is unmounted, unsubscribe
     // from the event with the `off` method
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-  return <Component {...pageProps} />
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;

f1948ceb39c640a441c4510cc605fbe6509f6f6d
Justin Torres <justin.torres811@gmail.com>
2021-11-07 14:49:43 -0500

    Created Contact Form

--- components/ContactUsForm/index.tsx
+++ components/ContactUsForm/index.tsx
@@ -6,25 +6,52 @@ const ContactUsForm = () => {
   };
 
   return (
-    <div className="p-10 rounded-lg w-1/2">
-      <form onSubmit={(e) => formHandler(e)} className="bg-gray-200">
-        <div>
-          <label htmlFor="">First Name</label>
-          <input className="" type="text" placeholder="First Name" />
+    <div className="rounded-lg w-full md:w-3/4  container mx-auto   ">
+      <h1 className="text-center p-5 text-2xl "  > 👋 Say HI!</h1>
+      <form
+        onSubmit={(e) => formHandler(e)}
+        className="rounded-lg bg-gray-100 flex flex-col gap-4 w-full p-5 md:p-20 "
+      >
+        <div className="flex justify-between flex-row gap-2 w-full ">
+          <div className="flex flex-col w-full">
+            <label htmlFor="">Your Name: </label>
+            <input
+              className="rounded-lg p-2"
+              type="text"
+              placeholder="First and Last Name"
+              required
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="">Company Name: </label>
+            <input className="rounded-lg p-2" type="text" placeholder="Company Name" required />
+          </div>
         </div>
-        <div>
-          <label htmlFor="">Last Name</label>
-          <input type="text" placeholder="Last Name" />
+
+        <div className="flex justify-between flex-row gap-2 w-full ">
+          <div className="flex flex-col w-full">
+            <label htmlFor="">Contact Email: </label>
+            <input className="rounded-lg p-2" type="email" placeholder="PlaceHolder@Email.com" required />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="">Contact Phone Number: </label>
+            <input className="rounded-lg p-2" type="tel" placeholder="(xxx)-xxx-xxxx" required />
+          </div>
         </div>
-        <div>
-          <label htmlFor="">Email</label>
-          <input type="email" placeholder="Email" />
+
+        <div className="flex justify-start flex-row gap-2 w-full  ">
+          <div className="flex flex-col gap-2 w-full ">
+            <label htmlFor="">Message: </label>
+            <textarea
+              placeholder="Please build our product!"
+              className="gap-2 rounded-lg h-40 w-full p-2 "
+            ></textarea>
+          </div>
+          
         </div>
-        <div>
-          <label htmlFor="">Message</label>
-          <textarea placeholder="Please build our product!"></textarea>
-        </div>
-        <Button>SUBMIT</Button>
+        <Button className="text-blue-600 bg-yellow-500 border-solid border-red-500">
+          SUBMIT
+        </Button>
       </form>
     </div>
   );

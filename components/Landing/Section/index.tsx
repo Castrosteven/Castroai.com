162e1e739311e46ebf98ce8135120e6e0b3490e1
Steven Casto <castrostevenyc@gmail.com>
2021-11-16 09:14:40 -0500

    Padding for mobile

--- components/Landing/Section/index.tsx
+++ components/Landing/Section/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 
 const Section: FC = ({ children }) => {
-  return <div className="container mx-auto max-w-6xl">{children}</div>;
+  return <div className="container mx-auto max-w-6xl p-5">{children}</div>;
 };
 export default Section;

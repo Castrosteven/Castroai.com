8f1e3e4242e87a46a834262d5a981ccd5e776883
Steven Casto <castrostevenyc@gmail.com>
2021-11-16 22:08:46 -0500

    Added Chat

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+
 const desc = "CASTROAI | Custom Web Application Development";
 const pageTitle = "Castro AI LLC";
 const twitterHandle = "";

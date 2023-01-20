3065a83a24ba07d65b297cbec4579bc2272cf89e
Steven Castro <steve-at-573687461965>
2023-01-20 14:23:34 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,13 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    container: {
-      
-    },
     extend: {
       fontFamily: {
         BroLink: ["BroLink"],
         Acumin:['Acumin']
       },
-      
-      colors: {
-        CdarkOrange: "#CE4912",
-        Corange: "#FF9D0A",
-        CdarkBlue: "#042C71",
-        Cblue: "#084387",
-        ClightBlue: "#29ABE2",
-        Cgray: "#B3B3B3",
-        CdarkGray: "#333333",
-        CdarkRed:'#A02B4F'
-      },
-      backgroundImage: {
-        'slide1': "url('/assets/brand_image.jpg')",
-        "techHeader":'url(/assets/TechnologiesHeader.png)'
-      },
     },
-    
   },
-
   variants: {
     extend: {},
   },
